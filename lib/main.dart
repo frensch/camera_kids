582ea58b62e174913d26955f96db8b690c220010
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2020-01-03 20:25:26 -0300

    adjusting aspect ratio of camera preview

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,11 @@ import 'package:native_device_orientation/native_device_orientation.dart';
 import 'package:gallery_saver/gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
-  Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.storage, PermissionGroup.camera]);
+  Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler()
+      .requestPermissions([PermissionGroup.storage, PermissionGroup.camera]);
 
   // Obtain a list of the available cameras on the device.
   final cameras = await availableCameras();
@@ -23,18 +22,19 @@ Future<void> main() async {
   final selfieCamera = cameras.last;
 
   //SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-   //   .then((_) {
-    runApp(
-      MaterialApp(
-        theme: ThemeData.dark(),
-        home: TakePictureScreen(
-          // Pass the appropriate camera to the TakePictureScreen widget.
-          camera: selfieCamera,
-        ),
+  //   .then((_) {
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: selfieCamera,
       ),
-    );
+    ),
+  );
   //});
 }
+
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
@@ -76,7 +76,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,34 +86,46 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             // If the Future is complete, display the preview.
             //return CameraPreview(_controller);
 
-            String url_image =
-                "assets/images/patinho.gif";
+            String url_image = "assets/images/patinho.gif";
             //    "https://frensch.s3.amazonaws.com/tenor.gif";
 
-            return NativeDeviceOrientationReader (
+            return NativeDeviceOrientationReader(
               builder: (context) {
-                NativeDeviceOrientation orientation = NativeDeviceOrientationReader.orientation(context);
+                NativeDeviceOrientation orientation =
+                    NativeDeviceOrientationReader.orientation(context);
                 print("Received new orientation: $orientation");
-                if (orientation == NativeDeviceOrientation.landscapeLeft || orientation == NativeDeviceOrientation.landscapeRight) {
-                  return Row(
+                if (orientation == NativeDeviceOrientation.landscapeLeft ||
+                    orientation == NativeDeviceOrientation.landscapeRight) {
+                  return Center( child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Expanded(child: Image.asset(url_image)),
-                      Expanded(
+                      Image.asset(url_image),
+                      Expanded(child:AspectRatio(
+                      aspectRatio: 1/_controller.value.aspectRatio,
+
                           child: RotatedBox(
-                              quarterTurns: (orientation == NativeDeviceOrientation.landscapeLeft ? 3 : 1),
-                              child: CameraPreview(_controller))),
+                              quarterTurns: (orientation ==
+                                      NativeDeviceOrientation.landscapeLeft
+                                  ? 3
+                                  : 1),
+                              child: CameraPreview(_controller)))),
                     ],
-                  );
+                  ));
                 } else {
                   return Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Expanded(child: Image.asset(url_image)),
+                      Image.asset(url_image),
                       Expanded(
-                          child: RotatedBox(
-                              quarterTurns: (orientation == NativeDeviceOrientation.portraitUp ? 0 : 2),
-                              child: CameraPreview(_controller))),
+                          child: AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+
+                child: RotatedBox(
+                              quarterTurns: (orientation ==
+                                      NativeDeviceOrientation.portraitUp
+                                  ? 0
+                                  : 2),
+                              child: CameraPreview(_controller)))),
                     ],
                   );
                 }
@@ -126,26 +137,22 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           }
         },
       ),
-
       floatingActionButton: FloatingActionButton(
-        child: (pressedButton ? Icon(Icons.all_inclusive) : Icon(Icons.camera_alt)),
+        child: (pressedButton
+            ? Icon(Icons.all_inclusive)
+            : Icon(Icons.camera_alt)),
         backgroundColor: (pressedButton ? Colors.grey : Colors.cyan),
         // Provide an onPressed callback.
         onPressed: () async {
-
           setState(() {
             pressedButton = true;
           });
-
-
 
           // Take the Picture in a try / catch block. If anything goes wrong,
           // catch the error.
           try {
             // Ensure that the camera is initialized.
             await _initializeControllerFuture;
-
-
 
             // Construct the path where the image should be saved using the
             // pattern package.
@@ -160,7 +167,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
             await _controller.takePicture(path);
 
             GallerySaver.saveImage(path);
-
 
             setState(() {
               pressedButton = false;
