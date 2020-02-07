d0e51bebfa3265b036ed7437e468841c3968b2ac
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2020-02-07 16:03:45 -0300

    version: 1.0.1+1, e bordas arredondadas

--- lib/CorrectRotation.dart
+++ lib/CorrectRotation.dart
@@ -2,93 +2,111 @@ import 'package:flutter/material.dart';
 import 'package:native_device_orientation/native_device_orientation.dart';
 import 'package:camera/camera.dart';
 
-
 class CorrectRotation extends StatefulWidget {
-
   final CameraController cameraController;
   final String urlImage;
   final Function takePicture;
   static bool pressedButton = false;
 
-  const CorrectRotation({Key key, this.cameraController, this.urlImage, this.takePicture}) : super(key: key);
+  const CorrectRotation(
+      {Key key, this.cameraController, this.urlImage, this.takePicture})
+      : super(key: key);
 
-
-  StateCorrectRotation createState()=> StateCorrectRotation(this.cameraController, this.urlImage, this.takePicture);
+  StateCorrectRotation createState() => StateCorrectRotation(
+      this.cameraController, this.urlImage, this.takePicture);
 }
 
-class StateCorrectRotation extends State<CorrectRotation>{
-
+class StateCorrectRotation extends State<CorrectRotation> {
   final CameraController cameraController;
   final String urlImage;
   final Function takePicture;
 
   StateCorrectRotation(this.cameraController, this.urlImage, this.takePicture);
 
-@override
+  @override
   Widget build(BuildContext context) {
     return NativeDeviceOrientationReader(
       builder: (context) {
         NativeDeviceOrientation orientation =
-        NativeDeviceOrientationReader.orientation(context);
+            NativeDeviceOrientationReader.orientation(context);
         print("Received new orientation: $orientation");
         if (orientation == NativeDeviceOrientation.landscapeLeft) {
-          return Center(child: Row(
+          return Center(
+              child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset(urlImage),
-              Expanded(child: AspectRatio(
-                  aspectRatio: 1 / cameraController.value.aspectRatio,
-                  child: RotatedBox(
-                      quarterTurns: 3,
-                      child: CameraPreview(cameraController)))),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.asset(urlImage)),
+              Expanded(
+                  child: AspectRatio(
+                      aspectRatio: 1 / cameraController.value.aspectRatio,
+                      child: RotatedBox(
+                          quarterTurns: 3,
+                          child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+        child:CameraPreview(cameraController))))),
               FloatingActionButton(
                 child: (CorrectRotation.pressedButton
                     ? Icon(Icons.all_inclusive)
                     : Icon(Icons.camera_alt)),
-                backgroundColor: (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
+                backgroundColor:
+                    (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
                 onPressed: takePicture,
               )
             ],
           ));
         } else if (orientation == NativeDeviceOrientation.landscapeRight) {
-          return Center( child: Row(
+          return Center(
+              child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               FloatingActionButton(
                 child: (CorrectRotation.pressedButton
                     ? Icon(Icons.all_inclusive)
                     : Icon(Icons.camera_alt)),
-                backgroundColor: (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
+                backgroundColor:
+                    (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
                 onPressed: takePicture,
               ),
-              Expanded(child:AspectRatio(
-                  aspectRatio: 1/cameraController.value.aspectRatio,
-                  child: RotatedBox(
-                      quarterTurns: 1,
-                      child: CameraPreview(cameraController)))),
-              Image.asset(urlImage),
-
+              Expanded(
+                  child: AspectRatio(
+                      aspectRatio: 1 / cameraController.value.aspectRatio,
+                      child: RotatedBox(
+                          quarterTurns: 1,
+                          child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child:CameraPreview(cameraController))))),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.asset(urlImage)),
             ],
           ));
         } else {
-          return Center( child: Column(
+          return Center(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset(urlImage),
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(100.0),
+                  child: Image.asset(urlImage)),
               Expanded(
                   child: AspectRatio(
                       aspectRatio: cameraController.value.aspectRatio,
                       child: RotatedBox(
-                          quarterTurns: (orientation ==
-                              NativeDeviceOrientation.portraitUp
-                              ? 0
-                              : 2),
-                          child: CameraPreview(cameraController)))),
+                          quarterTurns:
+                              (orientation == NativeDeviceOrientation.portraitUp
+                                  ? 0
+                                  : 2),
+                          child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child:CameraPreview(cameraController))))),
               FloatingActionButton(
                 child: (CorrectRotation.pressedButton
                     ? Icon(Icons.all_inclusive)
                     : Icon(Icons.camera_alt)),
-                backgroundColor: (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
+                backgroundColor:
+                    (CorrectRotation.pressedButton ? Colors.grey : Colors.cyan),
                 onPressed: takePicture,
               )
             ],
